7111d52df027588fa16a97abf165451310a8e755
Antti Varis <ajvari88@gmail.com>
2024-02-28 15:23:42 +0200

    hienosäätöä, kommentteja

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
+// Sivun latautuessa alustetaan laskuri.
 let counter = 0;
 
+
+// Funktio, jota kutsutaan Ota yhteyttä -sivulta, kun käyttäjä vie hiiren "Lähetä" -napin päälle.
 function pako(event) {
     event.target.removeAttribute('onmouseover');
     counter++;
 
+    // Päivitetään napin sisältöä laskurin kasvaessa.
     if (counter === 5) {
         event.target.innerHTML = '&#129300';
     } else if (counter === 10) {
@@ -15,15 +19,18 @@ function pako(event) {
         event.target.style.backgroundColor = 'OrangeRed';
         event.target.style.border = '1px solid OrangeRed';
     } else if (counter === 25) {
+        // Laskurin ollessa 25, nappi häviää kokonaan. Funktion suoritus päättyy ja nappi jää deaktivoiduksi.
         event.target.style.opacity = 0;
         event.target.style.cursor = 'default';
-        return
+        return;
     }
 
+    // Arvotaan luku väliltä 0-8.
     let arvonta = getRandomIntInclusive(0,8);
 
+    // Jos arvonnan arvo 0, poistetaan nappi hetkeksi näkyvistä.
     if (arvonta === 0) {
-        interval = setInterval(poisNakyvista, 50, event.target)
+        interval = setInterval(poisNakyvista, 50, event.target);
 
         timeout = setTimeout(() => {
             arvonta = getRandomIntInclusive(0,1);
@@ -34,24 +41,26 @@ function pako(event) {
                 event.target.style.left = '0%';
             }
             
-            interval = setInterval(takaisinNakyviin, 50, event.target)
+            interval = setInterval(takaisinNakyviin, 50, event.target);
             event.target.setAttribute('onmouseover', 'pako(event)');
 
-        }, getRandomIntInclusive(1000,2000))
+        }, getRandomIntInclusive(1000,2000));
 
-    } else {
-        let sijainti = parseInt(event.target.style.left.slice(0,-1))
+    }
+    // Jos arvonnan tulos on 1-8, siirretään nappia.
+    else {
+        let sijainti = parseInt(event.target.style.left.slice(0,-1));
         let uusi_paikka;
         
         if (sijainti < 50) {
-            uusi_paikka = getRandomIntInclusive5(50,100)
+            uusi_paikka = getRandomIntInclusive5(50,100);
             if (uusi_paikka - sijainti < 10) {
-                uusi_paikka = sijainti + 10
+                uusi_paikka = sijainti + 10;
             }
         } else {
-            uusi_paikka = getRandomIntInclusive5(0,50)
+            uusi_paikka = getRandomIntInclusive5(0,50);
             if (sijainti - uusi_paikka < 10) {
-                uusi_paikka = sijainti - 10
+                uusi_paikka = sijainti - 10;
             }
         }
     
@@ -65,48 +74,53 @@ function pako(event) {
 }
 
 
+// Funktio, joka siirtää nappia oikealle.
 function oikealle(button, uusi_paikka) {
     let sijainti = parseInt(button.style.left.slice(0,-1)) + 5;
 
     if (sijainti <= uusi_paikka) {
         button.style.left = sijainti + '%';
     } else {
-        pysayta()
+        clearInterval(interval);
     }
 }
 
+
+// Funktio, joka siirtää nappia vasemmalle.
 function vasemmalle(button, uusi_paikka) {
     let sijainti = parseInt(button.style.left.slice(0,-1)) - 5;
 
     if (sijainti >= uusi_paikka) {
         button.style.left = sijainti + '%';
     } else {
-        pysayta()
+        clearInterval(interval);
     }
 }
 
+
+// Funktio, joka vähentää napin opacitya.
 function poisNakyvista(button) {
     let opacity = parseFloat(button.style.opacity);
     if (opacity != 0) {
         button.style.opacity = opacity - 0.1;
     } else {
-        pysayta()
+        clearInterval(interval);
     }
 }
 
+
+// Funktio, joka lisää napin opacitya.
 function takaisinNakyviin(button) {
     let opacity = parseFloat(button.style.opacity);
     if (opacity != 1.0) {
         button.style.opacity = opacity + 0.1;
     } else {
-        pysayta()
+        clearInterval(interval);
     }  
 }
 
-function pysayta() {
-    clearInterval(interval)
-}
 
+// Funktio, joka palauttaa 5:llä jaollisen kokonaisluvun väliltä min - max.
 function getRandomIntInclusive5(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
@@ -114,9 +128,11 @@ function getRandomIntInclusive5(min, max) {
     while (x % 5 != 0) {
         x = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
     }
-    return x
+    return x;
 }
 
+
+// Funktio, joka palauttaa kokonaisluvun väliltä min-max.
 function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
